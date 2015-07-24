530fad5fe477ee11e9c9e90f94ad0e4bad55812e
Asad Butt <asadimtiazbutt@gmail.com>
2015-07-24 12:19:31 -0500

    records commit - account session made

--- client/records/records.js
+++ client/records/records.js
@@ -1,18 +1,44 @@
-Session.setDefault('counter', 0);
 
   Template.records.helpers({
     records: function () {
-      return PatientRecords.find({});
-    },
-
-    counter: function () {
-      return Session.get('counter');
+      return PatientRecords.find({username: Meteor.user().username}); // return ALL records in the DB
     }
   });
 
   Template.records.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    'click #add-record': function(){
+      // extract data from input fields AND insert a new record in the database
+      var recName = $('#new-record-name').val();
+      var recDescription = $('#new-record-description').val();
+      var recDate = $('#new-record-date').val();
+      var myFileId = Session.get('currFile');
+
+      if(!recName || !myFileId)
+        alert('ERROR: Record cannot be created without a Record Name OR valid File Attachment');
+      else
+      {
+        $('#myModal').modal('toggle');
+        $('#new-record-name').val('');
+        $('#new-record-description').val('');
+        $('#new-record-date').val('');
+
+        // insert the record
+        PatientRecords.insert({
+          name: recName,
+          description: recDescription,
+          dateOfRecord: recDate,
+          createdAt: new Date(),
+          file: myFileId,
+          owner: Meteor.userId(),
+          username: Meteor.user().username
+        });
+      }
+    },
+
+    // upload a file
+    'click #upload-file': function() {
+      var myFile = $('#choose-file').get(0).files[0];
+      var fileObj = Images.insert(myFile); // Images is the FS collection for files
+      Session.set('currFile', fileObj);
     }
   });
