9941f2380bdab36ff5e5d0b477bd42c5abde22a6
Jacob Waterman <jacobrwaterman@gmail.com>
2015-07-24 12:20:22 -0500

    made outer responsive and added side nav

--- client/script.js
+++ client/script.js
@@ -1,3 +1,34 @@
 Accounts.ui.config({
 	passwordSignupFields: "USERNAME_ONLY"
 })
+
+
+Template.outer.events({
+  "click #menu-toggle":function(event, template){
+  	event.preventDefault();
+    template.$("#wrapper").toggleClass("toggled");
+  },
+  "click #main-content, click":function(event, template){
+  	if($("#wrapper").hasClass("toggled"))
+  	{
+  	event.preventDefault();
+    template.$("#wrapper").toggleClass("toggled");
+  	}
+  },
+  "click #recordsLink":function(event,template){
+  	Router.go('/records');
+  	if($("#wrapper").hasClass("toggled"))
+  	{
+  	event.preventDefault();
+    template.$("#wrapper").toggleClass("toggled");
+  	}
+  },
+  "click #uploadLink":function(event,template){
+  	Router.go('/upload');
+  	if($("#wrapper").hasClass("toggled"))
+  	{
+  	event.preventDefault();
+    template.$("#wrapper").toggleClass("toggled");
+  	}
+  }
+});
